a1483d84a4073fa9fba76390fcce8b1dab595ecd
zzonle <bastianofedamaldonado@gmail.com>
2025-07-06 23:50:28 -0400

    ok

--- db/conexion.py
+++ db/conexion.py
@@ -1,14 +1,31 @@
+from colorama import init, Fore, Style
 from pymongo import MongoClient
 import getpass
+init(autoreset=True)
 
 def connect(db_name: str=""):
-    user = input("Usuario MongoDB: ")
-    pwd = getpass.getpass("Contraseña MongoDB: ") 
+    while True:
+        user = input(Fore.YELLOW + "Usuario MongoDB: " + Style.RESET_ALL)
+        pwd = getpass.getpass(Fore.YELLOW + "Contraseña MongoDB: " + Style.RESET_ALL)
+        if not user or not pwd:
+            print(Fore.RED + "Usuario y/o contraseña no pueden estar vacíos." + Style.RESET_ALL)
+            continue
+        
+        confirmar = input(Fore.CYAN + "¿Desea continuar con estos datos? (s/n): " + Style.RESET_ALL)
+        if confirmar.lower() != 's':
+            continue
+        else:
+            break
     host = "localhost"
     port = 27017
     uri = f"mongodb://{user}:{pwd}@{host}:{port}/?authSource=admin"
     cliente = MongoClient(uri)
-    return cliente[db_name]
+    try:
+            cliente.admin.command("ping")  
+            return cliente[db_name]
+    except Exception as e:
+            print(Fore.RED + "Error de credenciales y/o conectar con ComercioTech", str(e) + Style.RESET_ALL)
+            return None
 
 """
 def connect(db_name="comerciotech"):
