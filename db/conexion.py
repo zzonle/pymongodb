616947918e967b4a437f586ca34f5987776531be
zzonle <bastianofedamaldonado@gmail.com>
2025-07-06 22:09:40 -0400

    ok

--- db/conexion.py
+++ db/conexion.py
@@ -2,11 +2,10 @@ from pymongo import MongoClient
 import getpass
 
 def connect(db_name: str=""):
-    user = input("Usuario MongoDB [admin]: ") or "admin"
-    pwd = getpass.getpass("Contraseña MongoDB: ") or 'comercioTech123'
-    host = input("Host MongoDB [localhost]: ") or "localhost"
-    port_input = input("Puerto MongoDB [27017]: ")
-    port = int(port_input) if port_input else 27017
+    user = input("Usuario MongoDB: ")
+    pwd = getpass.getpass("Contraseña MongoDB: ") 
+    host = "localhost"
+    port = 27017
     uri = f"mongodb://{user}:{pwd}@{host}:{port}/?authSource=admin"
     cliente = MongoClient(uri)
     return cliente[db_name]
